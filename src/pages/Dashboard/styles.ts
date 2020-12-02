448cdb894b6dc0ff1bb9bc579023761ef00239be
Emerson Freitas <emersonfreitasn@gmail.com>
2020-12-02 16:10:16 -0500

    style input on error

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,10 @@
-import styled from 'styled-components';
+/* eslint-disable prettier/prettier */
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,7 +15,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -23,6 +28,14 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 1px solid #fff;
+    border-right: 0;
+
+    ${props =>
+    props.hasError &&
+    css`
+        border-color: #c53030;
+    `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -43,6 +56,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
@@ -76,7 +95,7 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 0 16px;
+      margin: 0 16px;
       flex: 1;
 
       strong {
